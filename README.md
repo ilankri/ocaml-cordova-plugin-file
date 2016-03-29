3eecebe5e0a0a832f0f44e986d4f372b82aeea99
Danny Willems <contact@danny-willems.be>
2016-03-29 17:11:48 +0200

    update to gen_js_api version

--- README.md
+++ README.md
@@ -15,6 +15,19 @@ on the device.
 
 Source: [cordova-plugin-file](https://github.com/apache/cordova-plugin-file)
 
+## Repository branches and tags
+
+We are migrating bindings from
+[js_of_ocaml](https://github.com/ocsigen/js_of_ocaml) (low level bindings) to
+[gen_js_api](https://github.com/lexifi/gen_js_api) (high level bindings).
+
+The gen_js_api binding allows to use *pure* ocaml types (you don't have to use
+the ## syntax from js_of_ocaml or Js.string type but only # and string type).
+
+The js_of_ocaml version is available in the branch
+[*js_of_ocaml*](https://github.com/dannywillems/ocaml-cordova-plugin-file/tree/js_of_ocaml)
+but we **recommend** to use the gen_js_api version which is the master branch.
+
 ## How to use ?
 
 See the official documentation
--- fileCordova.mli
+++ fileCordova.mli
@@ -3,45 +3,25 @@
 (* !! Read the documentation to know which directory are available on your
  * plateform !!
  *)
-class type file =
+class file : Ojs.t ->
   object
-    method applicationDirectory : Js.js_string Js.t Js.readonly_prop
-    method applicationStorageDirectory : Js.js_string Js.t Js.readonly_prop
-    method dataDirectory : Js.js_string Js.t Js.readonly_prop
-    method cacheDirectory : Js.js_string Js.t Js.readonly_prop
-    method externalApplicationStorageDirectory : Js.js_string Js.t Js.readonly_prop
-    method externalDataDirectory : Js.js_string Js.t Js.readonly_prop
-    method externalCacheDirectory : Js.js_string Js.t Js.readonly_prop
-    method externalRootDirectory : Js.js_string Js.t Js.readonly_prop
-    method tempDirectory : Js.js_string Js.t Js.readonly_prop
-    method syncedDataDirectory : Js.js_string Js.t Js.readonly_prop
-    method documentsDirectory : Js.js_string Js.t Js.readonly_prop
-    method sharedDirectory : Js.js_string Js.t Js.readonly_prop
+    inherit Ojs.obj
+    method application_directory                  : string
+    method application_storage_directory          : string
+    method data_directory                         : string
+    method cache_directory                        : string
+    method external_application_storage_directory : string
+    method external_data_directory                : string
+    method external_cache_directory               : string
+    method external_root_directory                : string
+    method temp_directory                         : string
+    method synced_data_directory                  : string
+    method documents_directory                    : string
+    method shared_directory                       : string
   end
 (* -------------------------------------------------------------------------- *)
 
 (* -------------------------------------------------------------------------- *)
-val file : unit -> file Js.t
-(* -------------------------------------------------------------------------- *)
-
-(* -------------------------------------------------------------------------- *)
-(* Type for different storage type *)
-type storage
-(* Get the directory in a string depending on the storage type using the ocaml
- * type storage.
- *)
-val storage_to_str : storage -> string
-
-val application_directory : storage
-val application_storage_directory : storage
-val data_directory : storage
-val cache_directory : storage
-val external_application_storage_directory : storage
-val external_data_directory : storage
-val external_cache_directory : storage
-val external_root_directory : storage
-val temp_directory : storage
-val synced_data_directory : storage
-val documents_directory : storage
-val shared_directory : storage
+val t : unit -> file
+[@@js.get "cordova.file"]
 (* -------------------------------------------------------------------------- *)
