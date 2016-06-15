39c319333a966931dbf4e4406226d59d1fe73b23
Danny Willems <contact@danny-willems.be>
2016-06-15 02:44:12 +0200

    Use functions instead of objects

--- cordova_file.mli
+++ cordova_file.mli
@@ -1,27 +1,29 @@
 (* -------------------------------------------------------------------------- *)
-(* Binding to the Javascript object *)
 (* !! Read the documentation to know which directory are available on your
  * plateform !!
  *)
-class file : Ojs.t ->
-  object
-    inherit Ojs.obj
-    method application_directory                  : string
-    method application_storage_directory          : string
-    method data_directory                         : string
-    method cache_directory                        : string
-    method external_application_storage_directory : string
-    method external_data_directory                : string
-    method external_cache_directory               : string
-    method external_root_directory                : string
-    method temp_directory                         : string
-    method synced_data_directory                  : string
-    method documents_directory                    : string
-    method shared_directory                       : string
-  end
-(* -------------------------------------------------------------------------- *)
-
-(* -------------------------------------------------------------------------- *)
-val t : unit -> file
-[@@js.get "cordova.file"]
+val application_directory                  : string
+[@@js.global "cordova.file.applicationDirectory"]
+val application_storage_directory          : string
+[@@js.global "cordova.file.applicationStorageDirectory"]
+val data_directory                         : string
+[@@js.global "cordova.file.dataDirectory"]
+val cache_directory                        : string
+[@@js.global "cordova.file.cacheDirectory"]
+val external_application_storage_directory : string
+[@@js.global "cordova.file.externalApplicationStorageDirectory"]
+val external_data_directory                : string
+[@@js.global "cordova.file.externalDataDirectory"]
+val external_cache_directory               : string
+[@@js.global "cordova.file.externalCacheDirectory"]
+val external_root_directory                : string
+[@@js.global "cordova.file.externalRootDirectory"]
+val temp_directory                         : string
+[@@js.global "cordova.file.tempDirectory"]
+val synced_data_directory                  : string
+[@@js.global "cordova.file.syncedDataDirectory"]
+val documents_directory                    : string
+[@@js.global "cordova.file.documentsDirectory"]
+val shared_directory                       : string
+[@@js.global "cordova.file.sharedDirectory"]
 (* -------------------------------------------------------------------------- *)
