869fe4bfaf22c31a202141f45667cb22acc4430f
Danny Willems <contact@danny-willems.be>
2016-06-17 15:04:46 +0200

    Use js.get instead of js.global because js.global is only for function

--- cordova_file.mli
+++ cordova_file.mli
@@ -2,28 +2,28 @@
 (* !! Read the documentation to know which directory are available on your
  * plateform !!
  *)
-val application_directory                  : string
-[@@js.global "cordova.file.applicationDirectory"]
-val application_storage_directory          : string
-[@@js.global "cordova.file.applicationStorageDirectory"]
-val data_directory                         : string
-[@@js.global "cordova.file.dataDirectory"]
-val cache_directory                        : string
-[@@js.global "cordova.file.cacheDirectory"]
-val external_application_storage_directory : string
-[@@js.global "cordova.file.externalApplicationStorageDirectory"]
-val external_data_directory                : string
-[@@js.global "cordova.file.externalDataDirectory"]
-val external_cache_directory               : string
-[@@js.global "cordova.file.externalCacheDirectory"]
-val external_root_directory                : string
-[@@js.global "cordova.file.externalRootDirectory"]
-val temp_directory                         : string
-[@@js.global "cordova.file.tempDirectory"]
-val synced_data_directory                  : string
-[@@js.global "cordova.file.syncedDataDirectory"]
-val documents_directory                    : string
-[@@js.global "cordova.file.documentsDirectory"]
-val shared_directory                       : string
-[@@js.global "cordova.file.sharedDirectory"]
+val application_directory                  : unit -> string
+[@@js.get "cordova.file.applicationDirectory"]
+val application_storage_directory          : unit -> string
+[@@js.get "cordova.file.applicationStorageDirectory"]
+val data_directory                         : unit -> string
+[@@js.get "cordova.file.dataDirectory"]
+val cache_directory                        : unit -> string
+[@@js.get "cordova.file.cacheDirectory"]
+val external_application_storage_directory : unit -> string
+[@@js.get "cordova.file.externalApplicationStorageDirectory"]
+val external_data_directory                : unit -> string
+[@@js.get "cordova.file.externalDataDirectory"]
+val external_cache_directory               : unit -> string
+[@@js.get "cordova.file.externalCacheDirectory"]
+val external_root_directory                : unit -> string
+[@@js.get "cordova.file.externalRootDirectory"]
+val temp_directory                         : unit -> string
+[@@js.get "cordova.file.tempDirectory"]
+val synced_data_directory                  : unit -> string
+[@@js.get "cordova.file.syncedDataDirectory"]
+val documents_directory                    : unit -> string
+[@@js.get "cordova.file.documentsDirectory"]
+val shared_directory                       : unit -> string
+[@@js.get "cordova.file.sharedDirectory"]
 (* -------------------------------------------------------------------------- *)
